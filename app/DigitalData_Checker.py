2c7143f75b8e87a5d03a1e7edbe6add8c19e3f99
zhekayur <zhekayurkevych@ukr.net>
2025-05-27 14:14:07 +0200

    removed GTM markdown as it not applicable for a streamlit lib

--- app/DigitalData_Checker.py
+++ app/DigitalData_Checker.py
@@ -10,29 +10,6 @@ import time
 # Set Streamlit config — MUST be first
 st.set_page_config(page_title="DigitalData Checker", page_icon="🔎", layout="centered")
 
-# Inject GTM into <head>
-st.markdown("""
-<!-- Google Tag Manager -->
-<script>
-(function(w,d,s,l,i){w[l]=w[l]||[];
-w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-j.async=true;
-j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TBPNMM9H');
-</script>
-""", unsafe_allow_html=True)
-
-# Inject GTM <noscript> into <body>
-st.markdown("""
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBPNMM9H"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-""", unsafe_allow_html=True)
 
 # Function to fetch digitalData using Selenium
 def get_digital_data(url):
